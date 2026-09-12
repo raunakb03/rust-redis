60d25929081b83ae5e511d00b61e6ac9b4b1013d
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2026-09-12 19:25:38 +0530

    Create TCP server on port 6379

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+    }
+}
