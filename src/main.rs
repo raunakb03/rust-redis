dc1334edef0bce9472b833d0733c3938d921a858
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2026-09-12 21:30:08 +0530

    Respond with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use tokio::net::TcpListener;
+use std::{error::Error};
+use tokio::{io::AsyncWriteExt, net::{TcpListener, TcpStream}};
+
+async fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn Error>>{
+    stream.write_all("+PONG\r\n".as_bytes()).await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -6,5 +12,10 @@ async fn main() -> std::io::Result<()> {
 
     loop {
         let (socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            if let Err(e) = handle_connection(socket).await {
+                eprintln!("Error reading from client: {}", e);
+            }
+        });
     }
 }
